6b509fd85a70ff9e5e836a28d034e5e496f2f47e
parmendra <parmendrak@360logica.com>
2015-05-19 23:53:22 +0530

    gvjh

--- MainActivity.cs
+++ MainActivity.cs
@@ -25,8 +25,7 @@ namespace Android222fdsfd
 			// and attach an event to it
 
 			Console.WriteLine ("Parmendra");
-			Console.WriteLine ("Choudhary");
-			Console.WriteLine ("Parmendra");
+
 			Console.WriteLine ("Parmendra");
 
 
